2239d5db017e2fbafd1f625c03fd12ccc72977e1
Felippe Regazio <felippe.moraes@zoho.com>
2019-06-11 22:21:21 -0300

    stable

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 (function(){
 	document.querySelectorAll('.swiper-container').forEach(function (s) {
-		let options = s.dataset.swiper ? JSON.parse(s.dataset.swiper) : {};
 		if (window.innerWidth <= 425) options.centeredSlides = true;
-		options.grabCursor = true;
-		options.freeMode = true;
-		options.pagination = {el: s.querySelector('.swiper-pagination')};
-		options.spaceBetween = 28;
+		var options = {
+			spaceBetween: 28,
+			grabCursor: true,
+			pagination: {
+				el: s.querySelector('.swiper-pagination'),
+				clickable:true
+			},
+		}
 		new Swiper(s, options);
 	});
 })();
