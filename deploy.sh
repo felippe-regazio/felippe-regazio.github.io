89b14adfa48e275d971d0643fd597b39965ea388
Felippe <felippe.moraes@zoho.com>
2020-11-12 15:02:30 -0300

    added deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
-git push origin :gh-pages
 git checkout -b gh-pages
+git reset --hard origin/master
 mv ./dist/* .
 git add .
 git commit -m "deploy"
 git push origin gh-pages --force
 git checkout master
-git branch -D gh-pages
